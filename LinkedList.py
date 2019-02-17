6b39c85b3579f3991d31cb85002a42f546d99c5a
Usman <sheikh_usman3@yahoo.com>
2019-02-17 18:19:08 -0500

    initial commit

---
+++ LinkedList.py
@@ -0,0 +1,17 @@
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+
+n1 = ListNode(1)
+n1.next = ListNode(2)
+n1.next.next = ListNode(3)
+n1.next.next.next = ListNode(4)
+
+
+
+
