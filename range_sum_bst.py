037dd96b6c64d9b41e7552fa1d1f0417b5aaf324
Usman <sheikh_usman3@yahoo.com>
2019-02-18 15:40:44 -0500

    bst

--- range_sum_bst.py
+++ range_sum_bst.py
@@ -17,3 +17,32 @@ class Solution(object):
         :type R: int
         :rtype: int
         """
+        def inorder_traversal( root, L, R):
+            if root != None:
+                inorder_traversal(root.left,L,R)
+                if (L <= root.val) and (R >= root.val):
+                    self.output += root.val
+                inorder_traversal(root.right,L,R)
+        self.output = 0
+        inorder_traversal(root,L,R)
+        return self.output
+
+
+ 
+
+# root = TreeNode(10)
+# root.left = TreeNode(5)
+# root.right = TreeNode(15)
+# root.left.left = TreeNode(3)
+# root.left.right = TreeNode(7)
+# root.right.right = TreeNode(18)
+# print(Solution().rangeSumBST(root,7,15))
+root = TreeNode(10)
+root.left = TreeNode(5)
+root.right = TreeNode(15)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(7)
+root.right.right = TreeNode(18)
+root.left.left.left = TreeNode(1)
+root.left.right.left = TreeNode(6)
+print(Solution().rangeSumBST(root,6,10))
