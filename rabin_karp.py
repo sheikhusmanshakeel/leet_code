519b5741c4b02643d1a5897fe6a601b43b991876
Usman <sheikh_usman3@yahoo.com>
2019-03-18 21:39:30 -0400

    rabin karp

---
+++ rabin_karp.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def rabin_karp_matcher(text, pattern, radix, prime):
+    n = len(text)
+    m = len(pattern)
+    h = np.power(radix,m-1) % prime
+    p = 0
+    t = 0
+    for i in range(m):
+        p = (radix*p+pattern[i]) % prime
+        t = (radix*t+text[i]) % prime
+    for s in range(n-m): # O(n-m)
+        if p == t:
+            if pattern == text[s+1:s+m]: # O(m) operation
+                print("pattern found")
+        if s < n-m:
+            t = (radix*(t - text[s+1]*prime))+ text[s+m+1] % prime
+
+
+text = "123456758"
+pattern = "75"
+rabin_karp_matcher(text,pattern,10,887)
